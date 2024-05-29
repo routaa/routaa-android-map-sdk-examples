2e8ee78c16721c0daba85c78f76c397e2aa745e1
mahdi-hosseinnion <45493154+mahdi-hosseinnion@users.noreply.github.com>
2024-05-29 23:43:47 +0330

    ci-cd: Get maven password from env variables

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,8 @@ dependencyResolutionManagement {
         maven {
             url  = uri("https://repo.routaa.ir/repository/maven-routaa-public")
             credentials {
-                username = ""
-                password = ""
+                username = project.hasProperty('ROUTAA_MAVEN_USERNAME') ? project.property('ROUTAA_MAVEN_USERNAME') : System.getenv('ROUTAA_MAVEN_USERNAME')
+                password = project.hasProperty('ROUTAA_MAVEN_PASSWORD') ? project.property('ROUTAA_MAVEN_PASSWORD') : System.getenv('ROUTAA_MAVEN_PASSWORD')
             }
         }
         google()
